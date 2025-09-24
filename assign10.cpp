8aed8a5e036ff4f2f74487e60ff02b1c85108a6c
PlainPresence <195653849+PlainPresence@users.noreply.github.com>
2025-09-24 15:29:55 +0530

    Create assign10.cpp

---
+++ assign10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "       1\n";
+    cout << "     1 2 3\n";
+    cout << "   1 2 3 4 5\n";
+    cout << " 1 2 3 4 5 6 7\n";
+
+    return 0;
+}
